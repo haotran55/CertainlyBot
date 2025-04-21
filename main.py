a8d60d3e8f499fed57c0873c7cf05813df67c273
haotran55 <haot54592@gmail.com>
2025-04-21 11:59:06 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+import requests
+import html
+from telebot import TeleBot
+from keep_alive import keep_alive
+keep_alive()
+bot = TeleBot("7379468791:AAFjogvlg3b_isuNyGBUYePw9uQ54-xAjms")
+
+# ID nhóm được phép sử dụng bot
+ALLOWED_GROUP_ID = -1002639856138
+GROUP_LINK = "https://t.me/HaoEsport01"  # Thay bằng link box chat của bạn
+
+def group_only(func):
+    def wrapper(message):
+        if message.chat.id == ALLOWED_GROUP_ID:
+            return func(message)
+        else:
+            bot.reply_to(
+                message,
+                f"❗ Bot chỉ hoạt động trong nhóm này: <a href=\"{GROUP_LINK}\">Box Chat</a>",
+                parse_mode="HTML",
+                disable_web_page_preview=True
+            )
+    return wrapper
+
+@bot.message_handler(commands=['video'])
+@group_only
+def random_video(message):
+    try:
+        res = requests.get("https://api.ffcommunity.site/randomvideo.php")
+        data = res.json()
+        video_url = data.get("url")
+
+        if video_url:
+            bot.send_chat_action(message.chat.id, "upload_video")
+            bot.send_video(message.chat.id, video=video_url, caption="Video gái xinh By @CertainllyBot")
+        else:
+            bot.send_message(message.chat.id, "Không lấy được video, thử lại sau nhé!")
+    except Exception as e:
+        bot.send_message(message.chat.id, "Đã xảy ra lỗi khi lấy video.")
+
+bot.polling()
